8f936c67922ad51f88164a41abef19d9b432a1a1
tygedavis <tyge.davis@gmail.com>
2019-11-05 15:14:36 -0700

    Made adjustments to Numbers.js and NumberButton.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const NumberButton = (props) => {
+  const {number} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       <button>
-        {props.button.value}
+        {number}
       </button>
     </>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,13 +10,13 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
-  const [numberState, setNumberState] = useState()
+  const [numberState] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {numbers.map(specialsBtn => <NumberButton btn={specialsBtn}/>)}
+       {numberState.map((specialsBtn, index) => <NumberButton key={index} btn={specialsBtn}/>)}
     </div>
   );
 };

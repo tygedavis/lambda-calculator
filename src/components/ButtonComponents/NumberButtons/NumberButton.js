f0afa53632fd0da4e4c903fd4c6a13ceddc48d9a
tygedavis <tyge.davis@gmail.com>
2019-11-05 14:55:00 -0700

    Added button and props to NumberButton.js. Included export default

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>
+        {props.button.value}
+      </button>
     </>
   );
 };
+
+
+export default NumberButton;

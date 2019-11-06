74d4637f20da73a03daaef2c9232587402924d0d
tygedavis <tyge.davis@gmail.com>
2019-11-05 18:15:21 -0700

    Added styling to the numbers buttons

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-   
+  
 
   return (
     <div className="container">
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Display = (props) => {
   console.log(props)
-  return <div className="display">{}</div>;
+  return <div className="display">0</div>;
 };
 
 export default Display;
